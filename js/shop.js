6f7316772d46cbb39a067d96807af0c4d86070ef
Nathan Wang <nathan.r.wang@gmail.com>
2016-04-17 12:25:29 -0700

    Revert "Revert "Add .gitignore, encrypt data""
    This reverts commit f3325ce4d2ba62d7657613d737f7371f495a6644.

--- js/shop.js
+++ js/shop.js
@@ -25,7 +25,7 @@ module.exports = function(game) {
             var backBtn = game.add.button(50, 50, "backButton", this.back);
             backBtn.scale.setTo(0.3, 0.3);
 
-            this.coinsLabel = game.add.text(game.world.width - 50, 50, "Coins: " + game.global.coins, { font: "36px Arial", fill: "#ffffff" });
+            this.coinsLabel = game.add.text(game.world.width - 50, 50, "Coins: " + game.global.get("coins"), { font: "36px Arial", fill: "#ffffff" });
             this.coinsLabel.anchor.setTo(1, 0);
 
             var shopGroup = game.add.group();
@@ -45,25 +45,20 @@ module.exports = function(game) {
             this.moreLivesText.setText("More Lives $" + this.getMoreLivesCost());
         },
         getMoreLivesCost: function() {
-            return ((window.localStorage.lives || 3) - 3)*20 + 10;
+            return (game.global.get("lives") - 3)*20 + 10;
         },
         moreLives: function() {
-            if (game.global.coins >= this.getMoreLivesCost()) {
-                this.updateCoins(game.global.coins - this.getMoreLivesCost());
-                if (window.localStorage.lives) {
-                    window.localStorage.lives++;
-                } else {
-                    window.localStorage.lives = 4;
-                }
+            if (game.global.get("coins") >= this.getMoreLivesCost()) {
+                this.updateCoins(game.global.get("coins") - this.getMoreLivesCost());
+                game.global.set("lives", game.global.get("lives") + 1);
                 this.setMoreLivesText();
             } else {
                 game.global.modal.showModal("notEnoughMoney");
             }
         },
         updateCoins: function(coins) {
-            game.global.coins = coins;
-            window.localStorage.coins = game.global.coins;
-            this.coinsLabel.setText("Coins: " + game.global.coins);
+            game.global.set("coins", coins);
+            this.coinsLabel.setText("Coins: " + game.global.get("coins"));
         },
         addClickListener: function(item, callback) {
             item.inputEnabled = true;
