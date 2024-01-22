a028a6a338b87c4d082bcbb39b6fa94526839f05
Nathan Wang <nathan.r.wang@gmail.com>
2024-01-21 21:59:04 -0500

    add walls; make player collide with walls

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import Phaser from "phaser";
 
 class GameScene extends Phaser.Scene {
+  /**
+   * Load any assets you might need here!
+   */
   preload() {
     this.load.image("player", "assets/player.png");
+
+    this.load.image("wallHorizontal", "assets/wallHorizontal.png");
+    this.load.image("wallVertical", "assets/wallVertical.png");
   }
 
+  /**
+   * Called once. Create any objects you need here!
+   */
   create() {
     // create the player sprite
     this.player = this.physics.add.sprite(250, 170, "player");
@@ -14,8 +23,39 @@ class GameScene extends Phaser.Scene {
 
     // create cursor keys
     this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.createWalls();
+
+    // Make the player collide with walls
+    this.physics.add.collider(this.player, this.walls);
   }
 
+  /**
+   * Creates the walls of the game
+   */
+  createWalls() {
+    this.walls = this.physics.add.staticGroup();
+
+    this.walls.create(10, 170, "wallVertical"); // Left
+    this.walls.create(490, 170, "wallVertical"); // Right
+
+    this.walls.create(50, 10, "wallHorizontal"); // Top left
+    this.walls.create(450, 10, "wallHorizontal"); // Top right
+    this.walls.create(50, 330, "wallHorizontal"); // Bottom left
+    this.walls.create(450, 330, "wallHorizontal"); // Bottom right
+
+    this.walls.create(0, 170, "wallHorizontal"); // Middle left
+    this.walls.create(500, 170, "wallHorizontal"); // Middle right
+    this.walls.create(250, 90, "wallHorizontal"); // Middle top
+    this.walls.create(250, 250, "wallHorizontal"); // Middle bottom
+  }
+
+  /**
+   * Phaser calls this function once a frame (60 times a second).
+   * 
+   * Use this function to move the player in response to actions,
+   * check for win conditions, etc.
+   */
   update() {
     this.movePlayer();
   }
