dbb946b826d1195b70e5b5f86ed63e8580608d5f
Nathan Wang <nathan.r.wang@gmail.com>
2024-01-21 21:54:39 -0500

    add movement to player

--- src/app.js
+++ src/app.js
@@ -11,6 +11,35 @@ class GameScene extends Phaser.Scene {
 
     // add gravity to make the player fall
     this.player.body.gravity.y = 500;
+
+    // create cursor keys
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    this.movePlayer();
+  }
+
+  /**
+   * Handles moving the player with the arrow keys
+   */
+  movePlayer() {
+    // check for active input
+    if (this.cursors.left.isDown) {
+      // move left
+      this.player.body.velocity.x = -200;
+    } else if (this.cursors.right.isDown) {
+      // move right
+      this.player.body.velocity.x = 200;
+    } else {
+      // stop moving in the horizontal
+      this.player.body.velocity.x = 0;
+    }
+
+    if (this.cursors.up.isDown && this.player.body.onFloor()) {
+      // jump if the player is on the ground
+      this.player.body.velocity.y = -320;
+    }
   }
 }
 
