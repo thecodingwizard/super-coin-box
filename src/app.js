7c442ea70c8cb3492cfd261aa0e364f54a9b0682
Nathan Wang <nathan.r.wang@gmail.com>
2024-02-27 16:45:40 -0500

    Add code for createWalls()

--- src/app.js
+++ src/app.js
@@ -22,3 +22,20 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+
+// Ignore this for now!! You'll use this for the createWalls() function.
+
+/*
+this.walls.create(10, 170, "wallVertical"); // Left
+this.walls.create(490, 170, "wallVertical"); // Right
+
+this.walls.create(50, 10, "wallHorizontal"); // Top left
+this.walls.create(450, 10, "wallHorizontal"); // Top right
+this.walls.create(50, 330, "wallHorizontal"); // Bottom left
+this.walls.create(450, 330, "wallHorizontal"); // Bottom right
+
+this.walls.create(0, 170, "wallHorizontal"); // Middle left
+this.walls.create(500, 170, "wallHorizontal"); // Middle right
+this.walls.create(250, 90, "wallHorizontal"); // Middle top
+this.walls.create(250, 250, "wallHorizontal"); // Middle botto
+*/
